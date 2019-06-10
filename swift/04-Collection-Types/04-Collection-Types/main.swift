2b83bd183a79941c8f096a4f365a8a73b75297ac
xiaoyuan <seyooe@gmail.com>
2019-06-10 12:19:36 +0800

    Add Collection-Types project.

---
+++ swift/04-Collection-Types/04-Collection-Types/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  04-Collection-Types
+//
+//  Created by xiaoyuan on 2019/6/10.
+//  Copyright © 2019 xiaoyuan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
